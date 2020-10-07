0eb79ce461857a782bc4cfdc7130c388b05ac796
Malte Blomqvist <blomqvist.malte@telia.com>
2020-10-07 10:01:28 +0200

    Test multiplayer connection

--- multiplayer/src/main.rs
+++ multiplayer/src/main.rs
@@ -2,7 +2,28 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let ip = "192.168.1.77:7878";
+    start_host(ip);
+}
+
+#[allow(clippy::unused_io_amount)]
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    let response = if buffer.contains(&(Messages::Accept as u8)) {
+        [Messages::Accept as u8]
+    } else {
+        [Messages::Decline as u8]
+    };
+
+    stream.write(&response).unwrap();
+    stream.flush().unwrap();
+}
+
+fn start_host(ip: &str) {
+    let listener = TcpListener::bind(ip).unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
@@ -11,18 +32,38 @@ fn main() {
     }
 }
 #[allow(clippy::unused_io_amount)]
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-
-    stream.read(&mut buffer).unwrap();
-
-    let contents = fs::read_to_string("multiplayer/hello.html").unwrap();
-    let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-        contents.len(),
-        contents
-    );
-
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+fn connect_client(ip: &str) {
+    if let Ok(mut stream) = TcpStream::connect(ip) {
+        println!("Connection established to: {}", ip);
+        stream.write(&[Messages::Accept as u8]).unwrap();
+        println!("Sent accept message...");
+        let mut buffer = [0; 1024];
+        if let Ok(_) = stream.read(&mut buffer) {
+            if buffer.contains(&(Messages::Accept as u8)) {
+                println!("Got accept message back!");
+            } else {
+                println!("No accept message back!");
+            }
+            println!("{}", String::from_utf8_lossy(&buffer[..]));
+        } else {
+            println!("Error reading response from stream!");
+        }
+    } else {
+        panic!("Error connecting to {}", ip);
+    }
 }
+
+#[repr(u8)]
+enum Messages {
+    Decline = 0x00,
+    Move = 0x01,
+    Undo = 0x02,
+    Accept = 0x03,
+    Checkmate = 0x04,
+    Draw = 0x05,
+    Resign = 0x06,
+}
+
+// impl Messages {
+//     fn get_move_message(mov: (u8, u8)) -> (u8, u8) {}
+// }
