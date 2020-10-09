b7b04e8abb844353d0528ca3bf676a276e7c40e4
Malte Blomqvist <blomqvist.malte@telia.com>
2020-10-09 07:03:58 +0200

    Thread test8

--- multiplayer/src/bin.rs
+++ multiplayer/src/bin.rs
@@ -3,8 +3,5 @@ use std::{env, thread, time::Duration};
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("Running {}...", args[0]);
-    if let Ok((message_sender, thread_handle)) = multiplayer::start_multiplayer(&args[1], &args[2])
-    {
-        thread_handle.join().unwrap();
-    }
+    multiplayer::start_multiplayer(&args[1], &args[2]);
 }
--- multiplayer/src/lib.rs
+++ multiplayer/src/lib.rs
@@ -11,10 +11,7 @@ pub mod chess_communicator;
 pub mod message;
 use message::{Message, MoveMessage};
 
-pub fn start_multiplayer(
-    connect_type: &str,
-    ip: &str,
-) -> Result<(Sender<[u8; 5]>, JoinHandle<()>)> {
+pub fn start_multiplayer(connect_type: &str, ip: &str) -> Result<()> {
     let is_host = match connect_type {
         "host" => true,
         "connect" => false,
@@ -31,25 +28,24 @@ pub fn start_multiplayer(
     }
 }
 
-fn start_host(ip: SocketAddrV4) -> Result<(Sender<[u8; 5]>, JoinHandle<()>)> {
+fn start_host(ip: SocketAddrV4) -> Result<()> {
     let listener = TcpListener::bind(ip).unwrap();
     println!("Started host at {}", ip);
-
     if let Ok((mut stream, client)) = listener.accept() {
         println!("Connection from {}", client);
-        stream.set_read_timeout(Some(Duration::from_secs(1)))?;
-        stream.set_write_timeout(Some(Duration::from_secs(1)))?;
-        stream.set_nonblocking(true)?;
 
-        let buffer = [255; 5];
+        let mut buffer = [255; 5];
         recieve_message(&mut stream, buffer, Some(Message::Accept))?;
         send_message(&mut stream, Message::Accept)?;
 
+        println!("{:?}", buffer.iter());
         let (sender, reciever) = channel::<[u8; 5]>();
         let handle = thread::spawn(move || {
-            std_loop(stream, reciever);
+            std_loop(stream, reciever, false);
         });
-        Ok((sender, handle))
+
+        handle.join().unwrap();
+        Ok(())
     } else {
         Err(Error::new(
             ErrorKind::Other,
@@ -58,12 +54,9 @@ fn start_host(ip: SocketAddrV4) -> Result<(Sender<[u8; 5]>, JoinHandle<()>)> {
     }
 }
 
-fn connect_client(ip: SocketAddrV4) -> Result<(Sender<[u8; 5]>, JoinHandle<()>)> {
+fn connect_client(ip: SocketAddrV4) -> Result<()> {
     if let Ok(mut stream) = TcpStream::connect(ip) {
         println!("Connection established to: {}", ip);
-        stream.set_read_timeout(Some(Duration::from_secs(1)))?;
-        stream.set_write_timeout(Some(Duration::from_secs(1)))?;
-        stream.set_nonblocking(true)?;
 
         send_message(&mut stream, Message::Accept)?;
 
@@ -72,21 +65,24 @@ fn connect_client(ip: SocketAddrV4) -> Result<(Sender<[u8; 5]>, JoinHandle<()>)>
 
         let (sender, reciever) = channel::<[u8; 5]>();
         let handle = thread::spawn(move || {
-            std_loop(stream, reciever);
+            std_loop(stream, reciever, true);
         });
-        Ok((sender, handle))
+        for i in 0..6 {
+            sender.send([i; 5]).expect("failed in sending message");
+            thread::sleep(Duration::from_millis(100))
+        }
+        handle.join().unwrap();
+        Ok(())
     } else {
         panic!("Error connecting to {}", ip);
     }
 }
 
-fn std_loop(mut stream: TcpStream, rx: Receiver<[u8; 5]>) {
+fn std_loop(mut stream: TcpStream, rx: Receiver<[u8; 5]>, mut is_sending: bool) {
     loop {
         let buffer = [255; 5];
-        println!("h");
-        let mut counter = 0;
-        if let Ok(message) = rx.try_recv() {
-            counter = 0;
+        if is_sending {
+            let message = [Message::Accept as u8; 5];
             let message_type = Message::from(message);
             if message_type != Message::Move {
                 if let Err(error) = send_message(&mut stream, message_type) {
@@ -97,20 +93,12 @@ fn std_loop(mut stream: TcpStream, rx: Receiver<[u8; 5]>) {
         } else {
             let result = recieve_message(&mut stream, buffer, None);
             if let Ok(message) = result {
-                if message != [255; 5] {
-                    counter = 0;
-                } else {
-                    counter += 1;
-                    println!("counter");
-                    if counter > 1000 {
-                        break;
-                    }
-                }
             } else {
                 println!("{}", result.unwrap_err().to_string());
             }
         }
-        thread::sleep(Duration::from_millis(1));
+        is_sending = !is_sending;
+        thread::sleep(Duration::from_millis(10));
     }
 }
 
@@ -166,7 +154,6 @@ fn recieve_message(
             ))
         }
     } else {
-        println!("{}", result.unwrap_err().to_string());
-        panic!("Error reading stream!");
+        Err(result.unwrap_err())
     }
 }
